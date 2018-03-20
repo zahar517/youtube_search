00de7b4ea5b607fa5397551bd2c75b1e473973ff
zahar517 <5173428@mail.ru>
2018-03-20 18:35:10 +0300

    Install react-native-elements and add Header Component

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
-export default () => null;
+import React, { Component } from "react";
+import { View } from "react-native";
+import { Header } from "react-native-elements";
+
+export class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: "#ddd" }}>
+        <Header
+          centerComponent={{
+            text: "Simple youtube Search",
+            style: { color: "white" },
+          }}
+          outerContainerStyles={{ backgroundColor: "#e62117" }}
+        />
+      </View>
+    );
+  }
+}
+
+export default App;
