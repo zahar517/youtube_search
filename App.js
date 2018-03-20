7eaf9dc7352a42d1e32d79dca7f7b51e6faaef5f
zahar517 <5173428@mail.ru>
2018-03-20 19:19:10 +0300

    Add search container

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { Header } from "react-native-elements";
+import { View, TextInput } from "react-native";
+import { Header, Button } from "react-native-elements";
 
 export class App extends Component {
+  state = {
+    inputValue: "",
+  };
+
+  searchInputChange = text => this.setState({ inputValue: text });
+
+  searchButtonClick = () => console.log(this.state.inputValue);
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "#ddd" }}>
@@ -13,9 +21,38 @@ export class App extends Component {
           }}
           outerContainerStyles={{ backgroundColor: "#e62117" }}
         />
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            value={this.state.inputValue}
+            onChangeText={this.searchInputChange}
+          />
+          <Button
+            buttonStyle={styles.searchButton}
+            title="Search"
+            onPress={this.searchButtonClick}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  searchContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  searchInput: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  searchButton: {
+    backgroundColor: "#e62117",
+  },
+};
 
 export default App;
